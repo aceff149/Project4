ee50f81e4cc6209db1c5d391525a8c60f14d7f20
Allie Efferson <allie.efferson@gmail.com>
2025-09-06 17:08:32 -0500

    update

--- server/routers/hats.js
+++ server/routers/hats.js
@@ -3,15 +3,18 @@ import db from "../dbConnection.js";
 
 const hatsRouter = express.Router();
 
-GET hats: http://localhost:4000/hats/
-Router.get ('/',async (req,res)=> {
-  console.log ("asdasdasdsa")
+
+// GET hats: http://localhost:4000/hats/
+hatsRouter.get("/", async (req, res) => {
+  console.log("Fetching all hats...");
   try {
-    const [result] = await db.query("SELECT * FROM Hats")
-    res.status(200).json(result)
+    const [result] = await db.query("SELECT * FROM Hats");
+    res.status(200).json(result);
   } catch (err) {
     console.log("Error retrieving Hats:", err);
     res.status(500).send("Server error while retrieving hats");
   }
-})
+  axios.get("http://localhost:4000/hats/")
+});
+
 export default hatsRouter;
